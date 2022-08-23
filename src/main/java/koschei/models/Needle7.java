62f1576d82956674c6e8cb019215e14a397088b2
evgeniy <evgeniy.shmelev.95@mail.ru>
2022-08-23 13:50:50 +0300

    add Deth8 Component

--- src/main/java/koschei/models/Needle7.java
+++ src/main/java/koschei/models/Needle7.java
@@ -1,9 +1,22 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class Needle7 {
+    
+    private Deth8 deth;
 
     @Override
     public String toString() {
         return ", смерть Кощея на игле :( ";
+    }
+
+    public Deth8 getDeth() {
+        return deth;
+    }
+
+    @Autowired
+    public void setDeth(Deth8 deth) {
+        this.deth = deth;
     }
 }
